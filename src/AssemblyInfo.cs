3232475438addbd5d7a9bcf49a59a81225af61b9
VSchuster <vincent_schuster@outlook.com>
2017-06-15 17:36:52 -0700

    QAQC_ReleaseUpdates_2

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -6,13 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Base Wind")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Portland State University")]
 [assembly: AssemblyProduct("base_wind.Properties")]
-[assembly: AssemblyCopyright("University of Wisconsin")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Landis-II-Foundation")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -29,5 +25,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
